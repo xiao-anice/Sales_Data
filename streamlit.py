f556bbb0dc810371e321a2c4a7dc9d322cf7b1c0
xiao-anice <56258444+xiao-anice@users.noreply.github.com>
2021-09-10 17:05:08 +0800

    add secrets.toml

--- streamlit.py
+++ streamlit.py
@@ -7,12 +7,33 @@ import plotly.express as px
 from PIL import Image
 from multiapp import MultiPage
 from apps import home, TPE30Data, TPE48Data, PVCData, KneeBraceData
+from google.oauth2 import service_account
+from gsheetsdb import connect
+
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+conn = connect(credentials=credentials)
+
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    return rows
+
+sheet_url = st.secrets["private_gsheets_url"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+# Print results.
+for row in rows:
+    st.write(f"{row.item-name} {row.item-description}:")
+
 
 st.markdown("""
 # Multi-Page App
 """)
 
-private_gsheets_url = "https://docs.google.com/spreadsheets/d/1aetVIVOXV634qoplPFPLOCIqUg4DEwbbsboSWcfk6nc/edit?usp=sharing"
 
 app = MultiPage()
 app.add_page('Home', home.app)
@@ -21,5 +42,3 @@ app.add_page('TPE48 Keywords', TPE48Data.app)
 app.add_page('PVC Keywords', PVCData.app)
 app.add_page('Knee Brace Keywords', KneeBraceData.app)
 app.run()
-
-
